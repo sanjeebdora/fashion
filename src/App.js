85c203d812eb9452e01cad4c62b3a94c6f91c635
Sanjeeb Dora <sanjeebdora@Sanjeebs-Mac-mini.local>
2023-05-07 08:55:56 +0530

    updated styles for navigation component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
-  document.title = 'Crown Clothing ';
+  document.title = 'Fashion Adress';
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
