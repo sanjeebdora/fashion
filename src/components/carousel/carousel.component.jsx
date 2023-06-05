85d04b29caabf617549be40a9a949d9c0f9e7ac0
Sanjeeb Dora <sanjeebdora@Sanjeebs-Mac-mini.local>
2023-06-05 14:10:18 +0530

    updated Strripe paymet method

--- src/components/carousel/carousel.component.jsx
+++ src/components/carousel/carousel.component.jsx
@@ -6,7 +6,7 @@ const Carousel = () => {
   //render() {
   return (
     <section className="relative bg-[url('https://i.ibb.co/P4V6Phf/derick-anies-h-DJT-ERr-B-w-unsplash.jpg')] bg-cover bg-center bg-no-repeat">
-      <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+      <div className="absolute inset-0  sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
 
       <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
         <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
